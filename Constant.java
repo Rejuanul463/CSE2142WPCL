e0fbe65c0129abb0d40b5640541930b797ecde66
Rejuanul463 <rejuanulhuq@yahoo.com>
2022-08-16 13:06:54 +0600

    done

--- Constant.java
+++ Constant.java
@@ -2,7 +2,7 @@ public class Constant{
     public static String showAll = "-a";
     public static String studentNameDivider = ",";
     public static String randomName = "-r";
-    public static String checker = "-c";
+    public static String counter = "-c";
     public static String add = "+";
     public static String check = "?";
     public static String completeMassage = "Data Loaded.";
@@ -11,7 +11,7 @@ public class Constant{
     public static String foundMassage = " word(s) found ";
     public static String dateFormat = "dd/mm/yyyy-hh:mm:ss";
     public static String founded = "We found it at index :";
-    public static String waPassed = "Wrong Argument Passed!";
+    public static String wrongArgumentPassed = "Wrong Argument Passed!";
     public static String notFound = "Name not Founded";
 
 }
--- StudentList.java
+++ StudentList.java
@@ -4,56 +4,56 @@ import java.util.*;
 public class StudentList {
 	public static void main(String[] args) {
 //		Check arguments
-		if(args[0].equals(Constant.showAll)) {
+		if(args[0].equals(Constant.showAll)) {											// to print all the students name
 			System.out.println(Constant.loadingMassage);			
 			try {
-				String studentNames[] = readData().split(Constant.studentNameDivider);			
-				for(String j : studentNames) { 
-					System.out.println(j); 
+				String studentNames[] = readData().split(Constant.studentNameDivider);	//read data and split on a string array		
+				for(String studentName : studentNames) { 
+					System.out.println(studentName); 
 				}
 			} catch (Exception e) {	} 
 			System.out.println(Constant.completeMassage);
 		}
-		else if(args[0].equals(Constant.randomName)) {
+		else if(args[0].equals(Constant.randomName)) {									// to print a Student name randomly
 			System.out.println(Constant.loadingMassage);			
 			try {
-				String studentNames[] = readData().split(Constant.studentNameDivider);
+				String studentNames[] = readData().split(Constant.studentNameDivider);	//read data and split on a string array
 				System.out.println(studentNames[new Random().nextInt(studentNames.length)]);
 			} catch (Exception e) { } 
 			System.out.println(Constant.completeMassage);			
 		}
-		else if(args[0].contains(Constant.add)) {
+		else if(args[0].contains(Constant.add)) {										// to add a name to the student list
 			System.out.println(Constant.loadingMassage);			
 			try {
-				writeData(args[0]);
+				writeData(args[0]);														// writing data to file method on 76th line
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 			System.out.println(Constant.completeMassage);	
 		}
-		else if(args[0].contains(Constant.check)) {
+		else if(args[0].contains(Constant.check)) {										// to check if name in given argument exists
 			System.out.println(Constant.loadingMassage);			
 			try {
 				String studentNames[] = readData().split(Constant.studentNameDivider);
-				for(int idx = 0; idx < studentNames.length ; idx++) {
-					if(studentNames[idx].equals(args[0].substring(1))) {
-						System.out.println(Constant.founded + idx);
-						break;
+				for(int index = 0; index < studentNames.length ; index++) {
+					if(studentNames[index].equals(args[0].substring(1))) {
+						System.out.println(Constant.founded + index);
+						break;															//break if found the name
 					}
-					if(idx == (studentNames.length-1)){
+					if(index == (studentNames.length-1)){
 						System.out.println(Constant.notFound);
 					}
 				}
 			} catch (Exception e) { } 
 			System.out.println(Constant.completeMassage);			
 		}
-		else if(args[0].contains(Constant.checker)) {
+		else if(args[0].contains(Constant.counter)) {									//count the number of students name
 			System.out.println(Constant.loadingMassage);			
 			try {
-				char studentNames[] = readData().toCharArray();			
+				char studentNames[] = readData().toCharArray();							//split all the charecters in a charecter array
 				int count = 1;
-				for(char c:studentNames) {
-					if(c == ',') {
+				for(char charecter:studentNames) {
+					if(charecter == ',') {
 						count++;
 					}
 				}
@@ -61,18 +61,18 @@ public class StudentList {
 			} catch (Exception e){ } 
 			System.out.println(Constant.completeMassage);				
 		}
-		else{
-			System.out.println(Constant.waPassed);
+		else{																			// to handle if wrong argument passed
+			System.out.println(Constant.wrongArgumentPassed);
 		}
 	}
-
+	//In order to read data;
 	static String readData() throws Exception{
 		BufferedReader bufferReader = new BufferedReader(
 			new InputStreamReader(
 				new FileInputStream(Constant.fileName)));
 		return bufferReader.readLine();
 	}
-
+	//In order to write data on student list
 	static void writeData(String args)throws Exception{
 		String nameLine = readData();
 				BufferedWriter bufferedWriter = new BufferedWriter(
