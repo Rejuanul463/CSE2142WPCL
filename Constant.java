a4f5d863ef14d4cae8e080a779b4fa9f89ea1045
Rejuanul463 <rejuanulhuq@yahoo.com>
2022-08-16 16:43:13 +0600

    done

--- Constant.java
+++ Constant.java
@@ -1,17 +1,31 @@
 public class Constant{
+    // argument to show all Student names
     public static String showAll = "-a";
+    // ',' is the student name devider
     public static String studentNameDivider = ",";
+    // argument to print a name randomly
     public static String randomName = "-r";
+    // argument to count number of words
     public static String counter = "-c";
+    // argument to add a new name 
     public static String add = "+";
+    // argument to check whether the name exists
     public static String check = "?";
+    // messege which will show when the process is complited
     public static String completeMassage = "Data Loaded.";
+    // messege to show if its loading 
     public static String loadingMassage = "Loading data ...";
+    // student text file name 
     public static String fileName = "students.txt";
+    // messege to show if the name is found
     public static String foundMassage = " word(s) found ";
+    // String formate of date
     public static String dateFormat = "dd/mm/yyyy-hh:mm:ss";
+    // messege to show if the name is founded
     public static String founded = "We found it at index :";
+    // messege to show when wrong argument is passed
     public static String wrongArgumentPassed = "Wrong Argument Passed!";
+    // messege to show when name isn't founded
     public static String notFound = "Name not Founded";
 
 }
